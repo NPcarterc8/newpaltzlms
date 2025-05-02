c3acab000894490b2259d8bad6e76b61a0ab5df3
NPcarterc8 <coreyc538@gmail.com>
2025-05-02 01:25:34 -0400

    trying to make in box still not working

--- src/main/java/name/coreycarter/classes/Sect.java
+++ src/main/java/name/coreycarter/classes/Sect.java
@@ -3,23 +3,26 @@ package name.coreycarter.classes;
 import java.util.List;
 
 public class Sect {
+
     private String sec_id;
     private Course course;
     private String teacher;
     private String start_time;
     private String end_time;
     private List<String> weekdays;
-    
+
     public Sect(String sec_id, String teacher, String start_time, String end_time, List<String> weekdays) {
-        this.sec_id= sec_id;
+        this.sec_id = sec_id;
         this.teacher = teacher;
         this.start_time = start_time;
         this.end_time = end_time;
         this.weekdays = weekdays;
     }
-    public void setcourse(Course course){
-        this.course= course;
+
+    public void setcourse(Course course) {
+        this.course = course;
     }
+
     public String getStartTime() {
         return start_time;
     }
@@ -27,25 +30,30 @@ public class Sect {
     public String getEndTime() {
         return end_time;
     }
+
     public List<String> getWeekdays() {
         return weekdays;
     }
+
     public Course getCourse() {
         return course;
     }
-    
+
     public String getName() {
         return course.getName();
     }
+
     public int getCredits() {
         return course.getCredits();
     }
 
-
     @Override
     public String toString() {
-        return course.getName() + " (" + start_time + " - " + end_time + ", " + String.join(", ", weekdays) + ")";
+        String name = course.getName();
+        if (course.getLab() != null && course.getLab() == this) {
+            name += "_Lab";
+        }
+        return name + " (" + start_time + " - " + end_time + ", " + String.join(", ", weekdays) + ")";
     }
-    
+
 }
-   
